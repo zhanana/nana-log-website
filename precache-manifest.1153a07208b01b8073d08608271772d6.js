28e5fcba375652dabbb861ec65b68dd0b2987f20
zhanana <15967137550@163.com>
2021-06-28 18:48:28 +0800

    deploy

---
+++ precache-manifest.1153a07208b01b8073d08608271772d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "241939bc64c4ede1675c",
+    "url": "/nana-log-website/css/app.ae5d08dd.css"
+  },
+  {
+    "revision": "e5a331fedcfcbfddbcb8e66e03351cd7",
+    "url": "/nana-log-website/index.html"
+  },
+  {
+    "revision": "241939bc64c4ede1675c",
+    "url": "/nana-log-website/js/app.10ec548d.js"
+  },
+  {
+    "revision": "0484c6c67a39f4ef7db0",
+    "url": "/nana-log-website/js/chunk-vendors.2e2c85f1.js"
+  },
+  {
+    "revision": "d3ec77f13eab1092983b20a629168f8f",
+    "url": "/nana-log-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/nana-log-website/robots.txt"
+  }
+]);
